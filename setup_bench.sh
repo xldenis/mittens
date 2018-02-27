407007343f372127eeb6e2c139d75a44f4dfd45d
Xavier Denis <xldenis@gmail.com>
2018-02-26 19:00:22 -0600

    Remove mysql pass

--- setup_bench.sh
+++ setup_bench.sh
@@ -1,3 +1,3 @@
-mysql -P 3000 -h 0.0.0.0 -pweak -uroot < config/init.sql
-mysql -P 5000 -h 0.0.0.0 -pweak -uroot < config/init_master.sql
-sysbench oltp_read_write prepare --mysql-user=root --mysql-password=weak --mysql-host='0.0.0.0' --mysql-port=5000 --time=100 --db-ps-mode=disable --threads=4 --tables=100
+mysql -P 3000 -h 0.0.0.0  -uroot < config/init.sql
+mysql -P 5000 -h 0.0.0.0  -uroot < config/init_master.sql
+sysbench oltp_read_write prepare --mysql-user=root --mysql-host='0.0.0.0' --mysql-port=5000 --time=100 --db-ps-mode=disable --threads=4 --tables=100
